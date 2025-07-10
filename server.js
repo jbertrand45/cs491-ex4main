e85481d020a177a25cdd984961ec252d370b4945
ayayrom <61670781+ayayrom@users.noreply.github.com>
2025-07-10 06:01:18 +0000

    testing implementation of a join button

--- server.js
+++ server.js
@@ -3,25 +3,57 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-let tokenStore = null;
+let tokens = {
+  players: [],
+  turn: null
+}
 
 app.use(express.static('public'));
 app.use(express.json());
 
-// save token
-app.post('/token', (req, res) => {
-  tokenStore = req.body;
-  console.log('token successfully stored', tokenStore);
+app.post('/join', (req, res) => {
+  const player = req.body;
+  if (tokens.players.length < 2) {
+    tokens.players.push(player);
+    tokens.turn = true; //TODO: optimize
+    console.log('Player joined:', player);
+    res.status(200).json({message: "Joined", players: tokens.players});
+  }
+  else {
+    return res.status(403).json({message: "Room full"});
+  }
 })
 
-// get token
-app.get('/token', (req, res) => {
-  if (!tokenStore) {
-    return res.status(404).send("test");
-  }
-  res.json(tokenStore);
+app.post('/leave', (req, res) => {
+  const player = req.body;
+  tokens.players = tokens.players.filter(existingPlayer => {
+    const sameUser = existingPlayer.user === player.user;
+    const sameBrowser = existingPlayer.browser === player.browser;
+    return !(sameUser && sameBrowser);
+  })
+  console.log('Player left:', player);
+  res.status(200).json({message: "Left", players: tokens.players});
 })
+
+app.get('/players', (req,res) => {
+  
+})
+
+// // save token from /token
+// app.post('/token', (req, res) => {
+//   tokens.players = req.body;
+//   console.log('token successfully stored', tokens.players);
+//   res.status(200).send("received token");
+// })
+
+// // get token from /token
+// app.get('/token', (req, res) => {
+//   if (tokens.players.length === 0) {
+//     return res.status(404).send("token not created");
+//   }
+//   res.json(tokens.players);
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`app listening on port ${port}`);
 })
