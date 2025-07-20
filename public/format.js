dbb46cafac8f677f9e50d165effab18714b601ba
ayayrom <hatomemes@gmail.com>
2025-07-20 02:18:47 -0700

    moved funcs to appropriate place

--- public/format.js
+++ public/format.js
@@ -21,22 +21,4 @@ function applyButtonFormat(button) {
   button.style.position = "relative"; // for tooltip positioning
 }
 
-/**
- * Helper function for viz
- */
-function showTooltip() {
-  const tip = this.querySelector("div");
-  tip.innerText = turn ? "x" : "o";
-  tip.style.visibility = "visible";
-}
-
-/**
- * Helper function for viz
- */
-function hideTooltip() {
-  const tip = this.querySelector("div");
-  tip.innerText = turn ? "x" : "o";
-  tip.style.visibility = "hidden";
-}
-
-export { applyButtonFormat, applyTextFormat, showTooltip, hideTooltip };
+export { applyButtonFormat, applyTextFormat };
