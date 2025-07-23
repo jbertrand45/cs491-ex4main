a517773cc467be81573f2948ecab32ba743f3f59
ayayrom <hatomemes@gmail.com>
2025-07-23 01:50:17 -0700

    added changeToken and role

--- public/states.js
+++ public/states.js
@@ -1,9 +1,10 @@
 //TokenState class 
 class tokenState {
-  constructor(user, agent, socketId) {
+  constructor(user, agent, socketId, role) {
     this.user = user;
     this.agent = agent;
     this.socketId = socketId; // Store socket ID
+    this.role = role; // 'x' first, 'o' second
     //this.lastActive = Date.now(); // Track last active time
   }
 }
@@ -19,13 +20,13 @@ class gameState {
 }
 
 //creates a JS object
-function setToken(user, socketId) {
+function setToken(user, socketId, role) {
   const a = navigator.userAgent; // browser name string
   var agent = "Firefox"; // the default, ff order is important
   if (a.indexOf("OPR") > 0) var agent = "Opera";
   else if (a.indexOf("Chrome") > 0) var agent = "Chrome";
   else if (a.indexOf("Safari") > 0) var agent = "Safari";
-  return new tokenState(user, agent, socketId);
+  return new tokenState(user, agent, socketId, role);
 }
 
 
@@ -45,14 +46,15 @@ async function postToken(token) {
 }
 
 async function removeToken(token) {
-  const res = await fetch('/leave', {
+  const req = await fetch('/leave', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(token)
   });
-  if (!res.ok) {
-    alert("Failed to leave: " + (await res.text()));
-    return false;
+  const res = await req.json();
+  if (!req.ok) {
+    alert("Failed to leave: " + res.message);
+    throw new Error("Failed to post token");
   }
   return true;
 }
@@ -70,6 +72,20 @@ async function getToken() {
   return await res.json();
 }
 
+async function changeToken(token) {
+  const req = await fetch('/change-token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(token)
+  });
+  const res = await req.json();
+  if (!res.ok) {
+    alert("Failed to change token.");
+    return false;
+  }
+  return true;
+}
+
 async function updateBoard(index) {
   const req = await fetch('/move', {
     method: 'POST',
@@ -84,4 +100,4 @@ async function updateBoard(index) {
   return res;
 }
 
-export { tokenState, setToken, postToken, removeToken, getToken, updateBoard };
+export { tokenState, setToken, postToken, removeToken, getToken, changeToken, updateBoard };
