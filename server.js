146bc1bce349871e6349f1b1aa02a02f57b81722
ayayrom <hatomemes@gmail.com>
2025-07-23 01:49:25 -0700

    added role to keep track of what player has

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ let tokens = {
 };
 
 let gameState = {
-  turn: null,
+  turn: 'x',
   started: null, // null means not joined, false means not started, true means started
   winner: null, // Track the winner, once winner is set, game is over
   board: Array(16).fill(null) // 4x4 board initialized to null
@@ -51,6 +51,9 @@ io.on('connection', (socket) => {
 app.post('/token', (req, res) => {
   const player = req.body;
   if ((!tokens.players.some(p => p.user === player.user))) {
+    if (player.user.match(/\d+/)) {
+      return res.status(400).json({ message: "No numbers in name allowed."});
+    }
     if (tokens.players.length < 2) {
       tokens.players.push(player);
     }
@@ -62,29 +65,25 @@ app.post('/token', (req, res) => {
     return res.status(400).json({ message: "Token already exists" });
   }
   console.log('token successfully stored', tokens.players);
-  res.status(200).json({ message: "Joined. Players: ", players: tokens.players, turn: gameState.turn });
+  res.status(200).json({ message: "Joined. Players: ", players: tokens.players });
+});
+
+app.post('/change-token', (req, res) => {
+  const newToken = req.body;
+  if (newToken.user === tokens.players[0].user) {
+    tokens.players[0] = newToken;
+  } else {
+    tokens.players[1] = newToken;
+  }
+  res.status(200);
 });
 
 app.post('/leave', (req, res) => {
   const player = req.body;
-  tokens.players = tokens.players.filter(existingPlayer => {
-    const sameUser = existingPlayer.user === player.user;
-    const sameBrowser = existingPlayer.browser === player.browser;
-    return !(sameUser && sameBrowser);
-  }
-  );
-  if (tokens.players.length === 0) {
-    gameState.turn = null;
-  }
-  else if (tokens.players.length === 1) {
-    gameState.turn = true;
-  }
-  gameState.started = null;
-  gameState.winner = null;
-  gameState.board = Array(16).fill(null); // Reset the board
   console.log('Player left:', player);
-  io.emit('gameState', gameState, `Player left: ${player.user}`)
+  io.emit('win', `${player.user} has left, ${tokens.players[0].user === player.user ? tokens.players[1].user : tokens.players[0].user} wins!`);
   res.status(200).json({ message: "Left", players: tokens.players });
+  resetVars();
 });
 
 // Coin flip to decide turn
@@ -92,20 +91,12 @@ app.post('/flip', (req, res) => {
   // first player gets to choose heads or tails
   const { input } = req.body;
   const flipResult = Math.floor(Math.random() * 2) === 1 ? 'H' : 'T';
-  if (input === flipResult) {
-    gameState.turn = true;
-  }
-  else {
-    gameState.turn = false;
-  }
   gameState.started = false; // preserving game state vars
-  const first = gameState.turn ? tokens.players[1].user : tokens.players[0].user;
+  const first = flipResult === input ? tokens.players[1].user : tokens.players[0].user;
   console.log(`Coin flipped: ${flipResult}. ${first} goes first.`);
-
   io.emit('flipResult', {
     message: `${tokens.players[1].user} chose ${input}, the coin flipped ${flipResult}, ${first} wins the first turn!`,
-    turn: gameState.turn,
-    order: first
+    turn: first,
   });
   res.status(200).json({ message: `Coin flipped ${flipResult}.` });
 });
@@ -126,11 +117,11 @@ app.post('/move', (req, res) => {
   if (gameState.board[index] !== null) {
     return res.status(400).json({ message: "Invalid move" });
   }
-  gameState.board[index] = gameState.turn ? 'x' : 'o'; // Set the player's mark
+  gameState.board[index] = gameState.turn; // Set the player's mark
   //CALCULATIONS FOR GAMESTATE GO HERE
-
-  const msg = `Player ${gameState.turn ? tokens.players[0].user : tokens.players[1].user} has placed their ${gameState.board[index]} on square ${index + 1}`;
-  gameState.turn = !gameState.turn;
+  const currentPlayer = tokens.players.find(p => p.role === gameState.turn)?.user;
+  const msg = `Player ${currentPlayer} has placed their ${gameState.turn} on square ${index + 1}`;
+  gameState.turn = gameState.turn === 'x' ? 'o' : 'x' // change turn when its x to o, o to x
   io.emit('gameState', gameState, msg); // important
   res.status(200).json({ board: gameState.board, turn: gameState.turn });
   if (checkWin()) resetVars();
@@ -179,7 +170,7 @@ function resetVars() {
   gameState = {
     started: null, // null means not joined, false means not started, true means started
     winner: null, // Track the winner, once winner is set, game is over
-    turn: null,
+    turn: 'x',
     board: Array(16).fill(null) // 4x4 board initialized to null
   };
   tokens = {
